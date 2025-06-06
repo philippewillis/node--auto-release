59cd937ce1b7dadc23a3abb087f1859194ab1fb4
philopian <philopian@gmail.com>
2025-06-06 14:47:48 -0700

    feat: Add CORS middleware to the Hono application

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 export function createApp() {
   const app = new Hono()
+
+  // Midleware
+  app.use('*', cors())
 
   app.get('/', (c) => c.text('Hono!'))
   return app
