30af17c9d5edb18153b04a9b33b31e6b1e7858eb
philopian <philopian@gmail.com>
2025-06-06 14:48:40 -0700

    feat: Add prettyJSON middleware to the Hono application

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { compress } from 'hono/compress'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
 
 export function createApp() {
   const app = new Hono()
@@ -10,7 +11,9 @@ export function createApp() {
   app.use('*', cors())
   app.use(compress())
   app.use(logger())
+  app.use(prettyJSON())
 
+  // Routes
   app.get('/', (c) => c.text('Hono!'))
   return app
 }
